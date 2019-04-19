2b8c0ca087f2c09887c2f2682cc4be20bc4c4f33
jeff sarlo <lgehrig4@yahoo.com>
2019-04-19 13:10:27 +0000

    add mongoose connect

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ const express           = require("express"),
       commentRoutes    = require("./routes/comments"),
       campgroundRoutes = require("./routes/campgrounds"),
       indexRoutes      = require("./routes/index");
-      
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v13", {useNewUrlParser: true});
+
+// mongoose.connect("mongodb://localhost:27017/yelp_camp_v13", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://lgehrig4:ReneeDBcode1!@cluster0-5yfak.mongodb.net/yelp_camp?retryWrites=true", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname +"/public"));
